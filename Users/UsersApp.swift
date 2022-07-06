055a052452f197c92b729adfa6ecb2ff88e7e9a8
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-06 10:22:01 +0200

    Initial Commit

---
+++ Users/UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersApp.swift
+//  Users
+//
+//  Created by Jan Andrzejewski on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
