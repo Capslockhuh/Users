f0c5b2f834966d5fadfa0d1b69993e83913e1bbf
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-06 10:46:27 +0200

    Started making function to load data

--- Users/ContentView.swift
+++ Users/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users = [User]()
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+   //         .onAppear(perform: )
+    }
+    
+    func loadUserData() {
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Error: Invalid URL")
+            return
+        }
     }
 }
 
